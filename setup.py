654fcf9a7940fb7f4d86a9ef0a0fb0e801f32330
Kwanghyun <senticat.lee@gmail.com>
2023-03-30 14:41:33 +0900

    init files

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name='Embed-base-model',
+    version='0.1.0',
+    license='Apache-2.0',
+    author='Daniel Lee',
+    author_email='rootuser.kr@gmail.com',
+    description='Python abstract class for embedding sentences',
+    url='https://github.com/asulikeit/embed-base-model',
+    packages=['embedmodel'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ]
+)
